ebd0b8589bfa9e47329702161d6da4578353272c
CalvinWilliams1012 <calvin@calvinwilliams.ca>
2025-10-07 00:11:58 -0400

    added book report output

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from stats import count_book_words
 from stats import character_count
+from stats import sort_character_count
 
 def get_book_text(filepath):
     with open(filepath) as file:
         return file.read()
 
-def main():
+def print_report(book_file):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_file}")
     book_text = get_book_text('books/frankenstein.txt')
     num_words = count_book_words(book_text)
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(character_count(book_text))
+    print("--------- Character Count -------")
+    sorted_char_count = sort_character_count(character_count(book_text))
+    for entry in sorted_char_count:
+        print(f"{entry['char']}: {entry['num']}")
+    print("============= END ===============")
+
+def main():
+    print_report('books/frankenstein.txt')
 
 main()
